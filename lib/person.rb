fd29c0815eb334f8d2aabb63dd7e358c1c833a3a
Emily Nguyen <emilyn0614@gmail.com>
2021-02-23 07:33:56 -0600

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,15 +1,4 @@
 class Person
-    def name=(name)
-        @name = name
-    end
-    def name
-        @name
-    end
-
-    def job=(job)
-        @job = job
-    end
-    def job
-        @job
-    end
+    attr_accessor(:name)
+    attr_accessor(:job)
 end
